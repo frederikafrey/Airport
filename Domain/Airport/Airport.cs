49ee4d3ba0c65d82f57a8bf605d2a278290f5e05
Helen Kalamäe <hekala@ttu.ee>
2020-09-12 12:40:21 +0300

    Domain kaustad baas

---
+++ Domain/Airport/Airport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Airport
+{   
+    //public sealed class Airport : Entity<AirportData>
+    //{
+    //    public Airport() : this(null) { }
+    //    public Airport(AirportData data) : base(data) { }
+    //}
+}
