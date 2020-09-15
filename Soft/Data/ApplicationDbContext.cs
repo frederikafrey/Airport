a6e8505aa4983277a50743e4ae71af44c9cfbe43
Mariliis Sinivee <masini@ttu.ee>
2020-09-15 10:04:00 +0300

    ApplicationDbContext

--- Soft/Data/ApplicationDbContext.cs
+++ Soft/Data/ApplicationDbContext.cs
@@ -6,9 +6,13 @@ namespace Airport.Soft.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            InitializeTables(builder);
+        }
+        internal void InitializeTables(ModelBuilder builder) => AirportDbContext.InitializeTables(builder);
     }
 }
