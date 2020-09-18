20d3b3d2e3fad09bb118ab104b517fe9e58c4a51
Ave Nilson <avnils@ttu.ee>
2020-09-18 13:43:08 +0300

    CopyTests and BaseTests

--- Tests/Aids/CopyTests.cs
+++ Tests/Aids/CopyTests.cs
@@ -1,19 +1,25 @@
-﻿//using Airport.Aids;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Airport.Aids;
+using Airport.Data.Passenger;
+using Airport.Facade.Passenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace Tests.Aids {
+namespace Airport.Tests.Aids
+{
 
-//    [TestClass] public class CopyTests : BaseTests {
+    [TestClass]
+    public class CopyTests : BaseTests
+    {
 
-//        [TestInitialize] public void TestInitialize() => type = typeof(Copy);
+        [TestInitialize] public void TestInitialize() => type = typeof(Copy);
 
-//        [TestMethod] public void MembersTest() 
-//        {
-//            var x = GetRandom.Object<ParticipantData>();
-//            var y = GetRandom.Object<ParticipantView>();
-//            TestArePropertyValuesNotEqual(x, y);
-//            Copy.Members(x, y);
-//            TestArePropertyValuesEqual(x, y);
-//        }
-//    }
-//}
+        [TestMethod]
+        public void MembersTest()
+        {
+            var x = GetRandom.Object<PassengerData>();
+            var y = GetRandom.Object<PassengersView>();
+            TestArePropertyValuesNotEqual(x, y);
+            Copy.Members(x, y);
+            TestArePropertyValuesEqual(x, y);
+        }
+    }
+}
--- Tests/BaseTests.cs
+++ Tests/BaseTests.cs
@@ -1,65 +1,67 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Airport.Aids;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Airport.Aids;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace Tests
-//{
-//    public class BaseTests
-//    {
-//        private static string notTested = "<{0}> is not tested";
-//        private static string notSpecified = "Class in not specified";
-//        private List<string> members { get; set; }
-//        protected Type type;
+namespace Airport.Tests
+{
+    public class BaseTests
+    {
+        private static string notTested = "<{0}> is not tested";
+        private static string notSpecified = "Class in not specified";
+        private List<string> members { get; set; }
+        protected Type type;
 
-//        [TestMethod]
-//        public void IsTested()
-//        {
-//            if (type == null) Assert.Inconclusive(notSpecified);
-//            var m = GetClass.Members(type, PublicBindingFlagsFor.DeclaredMembers);
-//            members = m.Select(e => e.Name).ToList();
-//            RemoveTested();
+        [TestMethod]
+        public void IsTested()
+        {
+            if (type == null) Assert.Inconclusive(notSpecified);
+            var m = GetClass.Members(type, PublicBindingFlagsFor.DeclaredMembers);
+            members = m.Select(e => e.Name).ToList();
+            RemoveTested();
 
-//            if (members.Count != 0) Assert.Fail(notTested, members[0]);
-//        }
+            if (members.Count != 0) Assert.Fail(notTested, members[0]);
+        }
 
-//        private void RemoveTested()
-//        {
-//            var tests = GetType().GetMembers().Select(e => e.Name).ToList();
+        private void RemoveTested()
+        {
+            var tests = GetType().GetMembers().Select(e => e.Name).ToList();
 
-//            for (var i = members.Count; i > 0; i--)
-//            {
-//                var m = members[i - 1] + "Test";
-//                var isTested = tests.Find(o => o == m);
+            for (var i = members.Count; i > 0; i--)
+            {
+                var m = members[i - 1] + "Test";
+                var isTested = tests.Find(o => o == m);
 
-//                if (!string.IsNullOrEmpty(isTested)) members.RemoveAt(i - 1);
-//            }
-//        }
-//        protected static void TestArePropertyValuesEqual(object obj1, object obj2)
-//        {
-//            foreach (var property in obj1.GetType().GetProperties())
-//            {
-//                var name = property.Name;
-//                var p = obj2.GetType().GetProperty(name);
-//                Assert.IsNotNull(p, $"No property with name '{name}' found.");
-//                var expected = property.GetValue(obj1);
-//                var actual = p.GetValue(obj2);
-//                Assert.AreEqual(expected, actual, $"For property '{name}'.");
-//            }
-//        }
-//        protected static void TestArePropertyValuesNotEqual(object obj1, object obj2) {
-//            foreach (var property in obj1.GetType().GetProperties()) {
-//                var name = property.Name;
-//                var p = obj2.GetType().GetProperty(name);
-//                Assert.IsNotNull(p, $"No property with name '{name}' found.");
-//                var expected = property.GetValue(obj1);
-//                var actual = p.GetValue(obj2);
+                if (!string.IsNullOrEmpty(isTested)) members.RemoveAt(i - 1);
+            }
+        }
+        protected static void TestArePropertyValuesEqual(object obj1, object obj2)
+        {
+            foreach (var property in obj1.GetType().GetProperties())
+            {
+                var name = property.Name;
+                var p = obj2.GetType().GetProperty(name);
+                Assert.IsNotNull(p, $"No property with name '{name}' found.");
+                var expected = property.GetValue(obj1);
+                var actual = p.GetValue(obj2);
+                Assert.AreEqual(expected, actual, $"For property '{name}'.");
+            }
+        }
+        protected static void TestArePropertyValuesNotEqual(object obj1, object obj2)
+        {
+            foreach (var property in obj1.GetType().GetProperties())
+            {
+                var name = property.Name;
+                var p = obj2.GetType().GetProperty(name);
+                Assert.IsNotNull(p, $"No property with name '{name}' found.");
+                var expected = property.GetValue(obj1);
+                var actual = p.GetValue(obj2);
 
-//                if (expected != actual) return;
-//            }
+                if (expected != actual) return;
+            }
 
-//            Assert.Fail("All properties are same");
-//        }
-//    }
-//}
+            Assert.Fail("All properties are same");
+        }
+    }
+}
