5a0d84a462eeec22af3d0b4accdb6cfbe906ccff
Mariliis Sinivee <masini@ttu.ee>
2020-10-01 10:53:59 +0300

    Commit

--- Aids/Safe.cs
+++ Aids/Safe.cs
@@ -1,38 +1,45 @@
 ﻿using System;
 
 namespace Airport.Aids {
-    public static class Safe {
+    public static class Safe
+    {
         private static readonly object key = new object();
         public static T Run<T>(Func<T> function, T valueOnException,
-            bool useLock = false) {
-            return useLock 
-                ? LockedRun(function, valueOnException) 
+            bool useLock = false)
+        {
+            return useLock
+                ? LockedRun(function, valueOnException)
                 : Run(function, valueOnException);
         }
-        public static void Run(Action action, bool useLock = false) {
+        public static void Run(Action action, bool useLock = false)
+        {
             if (useLock) LockedRun(action);
             else Run(action);
         }
 
-        private static T Run<T>(Func<T> function, T valueOnException) {
-            try 
+        private static T Run<T>(Func<T> function, T valueOnExeption)
+        {
+            try
             {
-               return function(); 
-            } 
-            catch (Exception e) 
+                return function();
+            }
+            catch (Exception e)
             {
                 Log.Exception(e);
-                return valueOnException;
+                return valueOnExeption;
             }
         }
 
-        private static T LockedRun<T>(Func<T> function, T valueOnException) {
-            lock (key) { return Run(function, valueOnException); }
+        private static T LockedRun<T>(Func<T> function, T valueOnExeption)
+        {
+            lock (key) { return Run(function, valueOnExeption); }
         }
-        private static void Run(Action action) {
+        private static void Run(Action action)
+        {
             try { action(); } catch (Exception e) { Log.Exception(e); }
         }
-        private static void LockedRun(Action action) {
+        private static void LockedRun(Action action)
+        {
             lock (key) { Run(action); }
         }
     }
