d0915167e40d9a834093da5898309cf544544558
Frederika Frey <frfrey@ttu.ee>
2020-11-06 16:30:04 +0200

    tegin juurde EditControlsForDropDownHtmlExtension2, idk kas see millekski kasulik ka on

---
+++ Pages/Extensions/EditControlsForDropDownHtmlExtension2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Airport.Pages.Extensions
+{
+    public static class EditControlsForDropDownHtmlExtension2
+    {
+        public static IHtmlContent EditControlsForDropDown<TModel, TResult>(
+            this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression,
+            IEnumerable<SelectListItem> items, SelectList seconddropDown)
+        {
+
+            var htmlStrings = EditControlsForDropDownHtmlExtension2.htmlStrings(htmlHelper, expression, items, seconddropDown);
+
+            return new HtmlContentBuilder(htmlStrings);
+        }
+
+        internal static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression, IEnumerable<SelectListItem> items, SelectList seconddropDown)
+        {
+            return new List<object> {
+                new HtmlString("<div class=\"form-group\">"),
+                htmlHelper.LabelFor(expression, new {@class = "text-dark"}),
+                htmlHelper.DropDownListFor(expression, items, new {@class = "form-control"}),
+                htmlHelper.ValidationMessageFor(expression, "", new {@class = "text-danger"}),
+                new HtmlString("</div>")
+            };
+        }
+    }
+}
