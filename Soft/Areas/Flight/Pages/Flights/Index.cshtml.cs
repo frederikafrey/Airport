1ed13b015f7d12690244739b441dbf09920b6304
Mariliis Sinivee <masini@ttu.ee>
2020-11-26 17:41:33 +0200

    Code cleaned

--- Soft/Areas/Flight/Pages/Flights/Index.cshtml.cs
+++ Soft/Areas/Flight/Pages/Flights/Index.cshtml.cs
@@ -11,36 +11,17 @@ namespace Airport.Soft.Areas.Flight.Pages.Flights
         private IApiCitiesRepository citiesRepository;
         private IApiCountriesRepository countriesRepository;
         
-
         public IndexModel(IFlightsRepository r, IApiCountriesRepository c, IApiCitiesRepository p) : base(r, c, p)
         {
             citiesRepository = p;
             countriesRepository = c;
         }
 
-        //public IList<FlightData> FlightData { get; set; }
-
-        //public async Task OnGetAsync(string sortOrder,
-        //    string id, string currentFilter, string searchString, int? pageIndex,
-        //    string fixedFilter, string fixedValue)
-        //{
-        //    SelectedId = id;
-        //    await GetList(sortOrder, currentFilter, searchString, pageIndex,
-        //        fixedFilter, fixedValue);
-        //}
-
         public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString,
             int? pageIndex, string fixedFilter, string fixedValue)
         {
-            var bb = await countriesRepository.GetAll();
-            var ee = bb.ElementAt(105);
-           // var qq = await citiesRepository.Get();
-            //var aa = await citiesRepository.GetAll();
             await GetList(sortOrder, currentFilter, searchString, pageIndex,
                 fixedFilter, fixedValue);
-            var ww = Items.ElementAt(0).StartCity;
-            var eee = citiesRepository.Get(ww);
-            //Items.ToList().ForEach(x => x.FinalCity = citiesRepository.Get(x.FinalCity).PlaceName);
         }
     }
 }
