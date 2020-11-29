bed9ade28c0a0fa15d212122d702f34101d7a81c
Mariliis Sinivee <masini@ttu.ee>
2020-11-29 16:36:03 +0200

    Api klassidel abstract eemaldatud ja lehed töötavad

--- Airport/Api/ApiCity/ApiCityProperties.cs
+++ Airport/Api/ApiCity/ApiCityProperties.cs
@@ -1,6 +1,6 @@
 ﻿namespace Airport.Data.Api.ApiCity
 {
-    public abstract class ApiCityProperties
+    public class ApiCityProperties
     {
         public string CityId { get; set; } //kas on vaja?
         public string PlaceName { get; set; }
--- Airport/Api/ApiCountry/ApiCountryProperties.cs
+++ Airport/Api/ApiCountry/ApiCountryProperties.cs
@@ -1,6 +1,6 @@
 ﻿namespace Airport.Data.Api.ApiCountry
 {
-    public abstract class ApiCountryProperties
+    public class ApiCountryProperties
     {
         public string Code { get; set; } //kas on vaja?
         public string Name { get; set; }
